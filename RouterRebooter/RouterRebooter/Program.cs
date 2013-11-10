aba99d4b8b6067232e79a3ea42b8416b55f16969
Alejandro Del Rincón <alexsp92@gmail.com>
2013-11-10 18:27:26 +0100

    Ahora pasamos los datos como parámetros

--- RouterRebooter/RouterRebooter/Program.cs
+++ RouterRebooter/RouterRebooter/Program.cs
@@ -1,44 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
 using System.Net.Sockets;
-using System.IO;
 using System.Threading;
-using System.Text.RegularExpressions;
+
 namespace RouterRebooter
 {
-    class Program
+    internal class Program
     {
-        const string IPADDRESS = "192.168.1.1";
-        const int PORT = 23;
-        const int TIMEOUT = 2000;
-        const string USER = "admin";
-        const string PASSWORD = "caca";
-        static void Main(string[] args)
+        private const int PORT = 23;
+        private const int TIMEOUT = 2000;
+
+        private static void Main(string[] args)
         {
             Program prog = new Program();
-            prog.InitConnection();
-            prog.connectHostandReboot(USER,PASSWORD);
+            if (args.Length == 3)
+            {
+                prog.InitConnection(args[0], args[1], args[2]);
+            }
+            else
+            {
+                Console.WriteLine("Tienes que pasar los argumentos: Direccion, Usuario y Contraseña.");
+                Console.Read();
+            }
         }
 
-        NetworkStream ns = null;
-        void InitConnection()
+        private NetworkStream ns = null;
+
+        private void InitConnection(string direccion, string user, string pass)
         {
-         
             try
             {
-
-                // Notifica que se ha producido un evento a un subproceso en espera.
                 AutoResetEvent connectDone = new AutoResetEvent(false);
-
-                // Proporciona conexiones de cliente para servicios de red TCP.
                 TcpClient tcpClient = new TcpClient();
 
                 //Comienza una solicitud asincrónica para una conexión a host remoto.
-                tcpClient.BeginConnect(IPADDRESS, PORT, new AsyncCallback(delegate(IAsyncResult ar)
+                tcpClient.BeginConnect(direccion, PORT, new AsyncCallback(delegate(IAsyncResult ar)
                 {
                     tcpClient.EndConnect(ar); connectDone.Set();
                 }), tcpClient);
@@ -50,34 +45,32 @@ namespace RouterRebooter
                     return;
                 }
 
-                ns = tcpClient.GetStream();
-                connectHostandReboot(USER, PASSWORD);
+                this.ns = tcpClient.GetStream();
+                connectHostandReboot(user, pass);
                 tcpClient.Close();
-
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-
         }
+
         public void connectHostandReboot(string user, string passwd)
         {
-
             // Conectando.....
             Byte[] output = new Byte[1024];
             String responseoutput = String.Empty;
             Byte[] cmd = System.Text.Encoding.ASCII.GetBytes("\n");
-            ns.Write(cmd, 0, cmd.Length);
+            //this.ns.Write(cmd, 0, cmd.Length);
 
             Thread.Sleep(1000);
             Int32 bytes = ns.Read(output, 0, output.Length);
             responseoutput = System.Text.Encoding.ASCII.GetString(output, 0, bytes);
-            // para comprobar que estamos conectados podemos controlar si el servidor nos devuelve una respuesta con la palabra login
-            Regex objToMatch = new Regex("login:");
-            if (objToMatch.IsMatch(responseoutput))
+            Console.Write(responseoutput);
+            if (responseoutput.EndsWith("Login: "))
             {
                 cmd = System.Text.Encoding.ASCII.GetBytes(user + "\r");
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(cmd));
                 ns.Write(cmd, 0, cmd.Length);
             }
             else
@@ -89,40 +82,42 @@ namespace RouterRebooter
             Thread.Sleep(1000);
             bytes = ns.Read(output, 0, output.Length);
             responseoutput = System.Text.Encoding.ASCII.GetString(output, 0, bytes);
+            Console.Write(responseoutput);
 
-            objToMatch = new Regex("Password");
-            if (objToMatch.IsMatch(responseoutput))
+            if (responseoutput.EndsWith("Password: "))
             {
                 cmd = System.Text.Encoding.ASCII.GetBytes(passwd + "\r");
+                //  Console.WriteLine(System.Text.Encoding.ASCII.GetString(cmd));
                 ns.Write(cmd, 0, cmd.Length);
             }
             else
             {
-                Console.WriteLine(IPADDRESS + " User not valid");
+                Console.WriteLine(" Usario no valido");
                 return;
             }
 
             Thread.Sleep(1000);
             bytes = ns.Read(output, 0, output.Length);
             responseoutput = System.Text.Encoding.ASCII.GetString(output, 0, bytes);
-
+            Console.Write(responseoutput);
             // controlamos el nombre de usuario que sea root
-            objToMatch = new Regex("root");
-            if (objToMatch.IsMatch(responseoutput))
-            {
 
+            if (responseoutput.EndsWith("> "))
+            {
                 // aqui podemos mandar el comando telnet que quieremos executar en el servidor.
-                cmd = System.Text.Encoding.ASCII.GetBytes("comando telenet" + "\r");
+                cmd = System.Text.Encoding.ASCII.GetBytes("reboot" + "\r");
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(cmd));
                 ns.Write(cmd, 0, cmd.Length);
             }
             else
             {
-                   Console.WriteLine(IPADDRESS + " Contraseña no valida");
+                Console.WriteLine(" Contraseña no valida");
                 return;
             }
 
             Thread.Sleep(1000);
             responseoutput = System.Text.Encoding.ASCII.GetString(output, 0, bytes);
+            Console.Write(responseoutput);
         }
     }
 }
